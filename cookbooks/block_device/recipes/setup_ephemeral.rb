139a4dcbc64341f33351ea71c525aada4a686f6c
insideview <sandeep.boyapati@insideview.com>
2012-11-21 17:38:47 +0530

    Update cookbooks/block_device/recipes/setup_ephemeral.rb

--- cookbooks/block_device/recipes/setup_ephemeral.rb
+++ cookbooks/block_device/recipes/setup_ephemeral.rb
@@ -41,7 +41,7 @@ lvm_device = "lvol0"
 if cloud == 'azure'
   ephemeral_mount_point = '/mnt/resource'
 else
-  ephemeral_mount_point = '/mnt'
+  ephemeral_mount_point = '/usr'
 end
 
 # Ubuntu systems using upstart require the 'bootwait' option, otherwise
